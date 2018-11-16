60c666c2b11fb2775ca4376ec4c1cf6d2703e86b
Liza Danilova <emdanilova_1@edu.hse.ru>
2018-11-16 16:49:36 +0300

    1

---
+++ function.py
@@ -0,0 +1,6 @@
+_users = {
+    'harry potter':{
+        'title': 'Harry Potter and Goblet of Fire',
+        'author': 'J.K.Rowling'
+    }
+}
--- newapp.py
+++ newapp.py
@@ -2,13 +2,22 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template('page1.html')
 
-app.run()
 
 @app.route('/<username>')
 def user_page(username):
-    return render_template('page1.html', user = username)
+    user_data=db.get_user(username)
+    return render_template('page1.html', user=user_data)
+
+
+@app.route('/new_book')
+def register_new_book(book_id):
+    return render_template('page1.html', book=book_id)
+
+
+app.run()
 
